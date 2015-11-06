72741751eeeb48fe7b754cfe1efff6ca468d24a0
Zarnosch <janoszwei@yahoo.de>
2015-11-06 23:42:52 +0100

    Added some sprites with initial scripts and a gamecontroller script which includes seasons and time

---
+++ Forge of Sacrifice Saga/Assets/Scripts/FarmController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FarmController : MonoBehaviour {
+    public int MaxWorker;
+    public int CurrentWorker;
+    public int Productivity;
+    public bool IsActive;
+    public int foodPerDay;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(CurrentWorker > MaxWorker)
+        {
+            CurrentWorker = MaxWorker;
+        }
+
+        foodPerDay = CurrentWorker * Productivity;
+    }
+}
---
+++ Forge of Sacrifice Saga/Assets/Scripts/GameController.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+    // Some Stuff for the Timecicle
+    public int Day, Week, Month, Year, Hour;
+    public int GameSpeed;
+    // timeHandling
+    private float lastGameTime;
+    private int lastDay, lastWeek, lastMonth, lastYear, lastHour;
+
+    // Use this for initialization
+    void Start () {
+        Day = 1; Week = 1; Month = 1; Year = 1; Hour = 1;
+        lastDay = 1; lastWeek = 1; lastMonth = 1; lastYear = 1; lastHour = 0;
+        lastGameTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Some season handling
+        lastHour = Hour;
+        // 24 Seconds = 1 Day;
+        Hour = (int)(Time.time * GameSpeed - lastGameTime * GameSpeed);
+        lastDay = Day;
+        lastWeek = Week;
+        lastMonth = Month;
+        lastYear = Year;
+        if(!(Hour < 24))
+        {
+            lastGameTime = Time.time;
+            Hour = 1;
+            lastDay = Day;
+            Day++;
+        }
+        if(Day > 7)
+        {
+            Day = 1;
+            lastWeek = Week;
+            Week++;
+        }
+        if (Week > 4)
+        {
+            Week = 1;
+            lastMonth = Month;
+            Month++;
+        }
+        if (Month > 12)
+        {
+            Month = 1;
+            lastYear = Year;
+            Year++;
+        }
+
+        // Code, which runs once per hour
+        if(lastHour != Hour)
+        {
+            //Debug.Log("Hour change " + Hour);
+        }
+        // Code, which runs once per day
+        if (lastDay != Day)
+        {
+            //Debug.Log("Day change");
+        }
+        // Code, which runs once per week
+        if (lastWeek != Week)
+        {
+            //Debug.Log("Week change");
+        }
+        // Code, which runs once per month
+        if (lastMonth != Month)
+        {
+            //Debug.Log("Month change");
+        }
+        // Code, which runs once per year
+        if (lastYear != Year)
+        {
+            //Debug.Log("Year change");
+        }
+    }
+}
