f4a620d241de0dfa1956c532795f1ac918009051
12Cy <christian.sandkaemper@gmail.com>
2015-11-07 03:05:34 +0100

    Alle Daten

---
+++ Forge of Sacrifice Saga/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonController : MonoBehaviour {
+    public GameObject popup;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void closeButton()
+    {
+        popup.SetActive(false);
+    }
+}
---
+++ Forge of Sacrifice Saga/Assets/Scripts/clickforhouseinfos.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class clickforhouseinfos : MonoBehaviour {
+    public GameObject PopupWindow;
+	// Use this for initialization
+	void OnMouseDown()
+    {
+        Debug.Log("kay bro");
+        if(PopupWindow)
+        {
+            PopupWindow.SetActive(true);
+        }
+    }
+    void Update()
+    {
+        
+        
+    }
+
+   
+}
